d81454cb5f9bfe0361555105b0115e0524e97307
richnologies <me@richnologies.io>
2017-12-14 01:48:32 +0100

    chore(global): upgrade dependencies

---
+++ render.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const ejs = require('ejs');
+
+const files = [
+  'index',
+  'gardabateas'
+];
+
+files.forEach(filename => {
+  const content = fs.readFileSync(`./views/${filename}.ejs`, 'utf-8');
+
+  fs.writeFileSync(`./public/${filename}.html`, ejs.render(content, {
+    filename: `./views/${filename}.html`
+  }), 'utf-8');
+});
